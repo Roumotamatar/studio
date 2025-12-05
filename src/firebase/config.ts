ceb70e4a732b672a60c50848246a82c6f9a56ef8
Pompom <pompomtamatar9832@gmail.com>
2025-12-05 16:38:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "skinwise-2-12423071-f1727",
+  "appId": "1:576049475114:web:b43ef8e24db3640da75d53",
+  "apiKey": "AIzaSyA4-JddCZzc5kzK1Rxoq2ajnmQYB9y-tKI",
+  "authDomain": "skinwise-2-12423071-f1727.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "576049475114"
+};
